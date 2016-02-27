c237e6844508dbeb915f0349f1be24f0c434ae8a
Christine Cha <hello@christinecha.com>
2016-02-27 02:09:48 -0500

    connected Folio variables to the React App.

--- src/components/Gridfolio.jsx
+++ src/components/Gridfolio.jsx
@@ -1,12 +1,69 @@
 "use strict"
 
 import React from 'react'
+import { Folio, FolioStyle } from '../myGridfolio.js'
 
 export class Gridfolio extends React.Component{
+
+  getBlocks(row) {
+    return row.map((block, i) => {
+
+      let padding = FolioStyle.blockOuterPadding
+      let blockWidth = (parseInt(window.innerWidth) / row.length) - (padding * 2)
+      let blockHeight = blockWidth
+      let blockTitlePosition = (blockHeight / 2) - ((block.titleFontSize || FolioStyle.blockTitleFont.fontSize) / 2)
+
+      let blockStyle = {
+        outer: {
+          width: blockWidth + 'px',
+          height: blockHeight + 'px',
+          padding: padding + 'px',
+        },
+        inner: {
+          backgroundColor: FolioStyle.blockBackgroundColor,
+          backgroundImage: block.image,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100%',
+          height: '100%',
+          textAlign: FolioStyle.blockTitleTextAlign
+        },
+        title: {
+          color: block.titleColor || FolioStyle.blockTitleFont.color,
+          height: blockHeight - blockTitlePosition + 'px',
+          paddingTop: blockTitlePosition + 'px',
+          fontSize: block.titleFontSize || FolioStyle.blockTitleFont.fontSize + 'px',
+          fontFamily: FolioStyle.blockTitleFont.fontFamily,
+          textTransform: FolioStyle.blockTitleFont.textTransform,
+          fontWeight: FolioStyle.blockTitleFont.fontWeight,
+          backgroundColor: block.isTinted ? FolioStyle.blockTint : null
+        }
+      }
+
+      return (
+        <div
+          style={ blockStyle.outer }
+          className="folio-block">
+          <div style={ blockStyle.inner }>
+            <div style={ blockStyle.title }>
+              {block.title}
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  getRows() {
+    return Folio.map((row, i) => {
+      return this.getBlocks(row)
+    })
+  }
+
   render() {
     return (
       <div>
-        Gridfolio
+        { this.getRows() }
       </div>
     )
   }
--- src/myGridfolio.js
+++ src/myGridfolio.js
@@ -1,35 +1,45 @@
 "use strict"
 
-let GridfolioStyle = {
-  body {                          // overall page
-    backgroundColor: "#f1f1f2",   // default light gray
-  },
-  gridBlock {                     // each grid "block"
-    border: "none",               // default "none" (no border at all)
-    borderRadius: "0",            // default "0" (sharp corners)
-    sizeRatio: "1:1",             // default "none" (square) - formatted "width:height"
+import React from 'react'
+
+export const FolioStyle = {
+  blockOuterPadding: 20,
+  blockInnerPadding: 10,
+  blockBackgroundColor: '#f1f1f2',
+  blockTint: 'rgba(0,0,0,0.4)',
+  blockTitleTextAlign: 'center',
+  blockTitleFont: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'Geo', // ! Remember to add any font resources to index.html in a link tag.
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
   }
 }
 
-let Gridfolio = [
+export const Folio = [
   [ // Row 1
     { // Row 1, Item 1
       title: "Circlematch",
       brief: "An in-browser and iOS native game written in React and React-Native.",
-      link: "https://google.com",
-      image: "https://image.com"
+      link: "https://christinecha.github.io/circlematch",
+      image: "url('./src/assets/CircleMatch.png')",
+      isTinted: true
     },
     { // Row 1, Item 2
       title: "Christine Cha",
       brief: null,
       link: null,
-      image: "https://image.com"
+      image: "https://image.com",
+      titleColor: "#b1b1b2",
+      titleFontSize: 40,
     },
     { // Row 1, Item 3
       title: "NYC Jazz App",
       brief: "Data scraped with NodeJS + Cheerio, built into a React web app and Python Twitter bot.",
       link: "https://google.com",
-      image: "https://image.com"
+      image: "https://image.com",
+      whRatio: 1
     }
   ],
   [ // Row 2
@@ -40,21 +50,21 @@ let Gridfolio = [
       image: null
     },
     { // Row 2, Item 2
-      title: "font-awesome icon for github",
+      title: <i className="fa fa-twitter"></i>,
       brief: null,
-      link: "https://google.com",
+      link: "https://twitter.com/christinechanyc",
       image: "https://image.com"
     },
     { // Row 2, Item 2
-      title: "font-awesome icon for github",
+      title: <i className="fa fa-github"></i>,
       brief: null,
-      link: "https://google.com",
+      link: "https://github.com/christinechanyc",
       image: "https://image.com"
     },
     { // Row 2, Item 2
-      title: "font-awesome icon for github",
+      title: <i className="fa fa-instagram"></i>,
       brief: null,
-      link: "https://google.com",
+      link: "https://instagram.com/christinechanyc",
       image: "https://image.com"
     },
     { // Row 2, Item 1
